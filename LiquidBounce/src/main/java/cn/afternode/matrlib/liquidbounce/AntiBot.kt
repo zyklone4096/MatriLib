af15125ab9e8d87aa2e086ee3f366a24f37c5745
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2023-03-05 10:48:34 +0800

    trying to generate bots to bypass default AntiBot settings of LiquidBounce

--- LiquidBounce/src/main/java/cn/afternode/matrlib/liquidbounce/AntiBot.kt
+++ LiquidBounce/src/main/java/cn/afternode/matrlib/liquidbounce/AntiBot.kt
@@ -1,8 +1,14 @@
 package cn.afternode.matrlib.liquidbounce
 
 import cn.afternode.matrlib.liquidbounce.LiquidColors.stripColor
+import org.bukkit.GameMode
+import org.bukkit.Location
 import org.bukkit.entity.Entity
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.HumanEntity
 import org.bukkit.entity.Player
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 object AntiBot {
     /**
@@ -32,5 +38,20 @@ object AntiBot {
         if (from != null) fromName = from.name
 
         return entity.name.isEmpty() || entity.name == fromName
+    }
+
+    /**
+     * Try to generate a bot entity to bypass the default settings
+     */
+    @JvmStatic
+    fun generateBot(loc: Location, name: String, invisible: Boolean = true): Player {
+        val entity = loc.world.spawnEntity(loc, EntityType.PLAYER) as Player
+        entity.setGravity(false)
+        if (invisible) entity.addPotionEffect(PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 255, false, false))
+        entity.customName = name
+        entity.playerListName = name
+        entity.displayName = name
+        entity.gameMode = GameMode.SURVIVAL
+        return entity
     }
 }
